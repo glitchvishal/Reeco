68e63fe33e236d159d340e9d6c5174cc8470b90d
vishal kr. singh <82654082+glitchvishal@users.noreply.github.com>
2023-11-28 00:06:07 +0530

    fix ui

--- src/component/OrderPage.js
+++ src/component/OrderPage.js
@@ -5,17 +5,28 @@ import {
   Button,
   Typography,
   Divider,
-  styled,
+  // styled,
   Paper,
   Box,
+  TextField,
 } from "@material-ui/core";
 import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 import { useDispatch, useSelector } from "react-redux";
 import OrderList from "./OrderList";
 import Header from "./Header";
 import ConfirmationDialog from "./ConfirmationDialog";
 import { APPROVE_ORDER, SET_DIALOG_OPEN } from "../redux/actions";
 import { Add as AddIcon } from "@material-ui/icons";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import IcecreamIcon from "@mui/icons-material/Icecream";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import TapasIcon from "@mui/icons-material/Tapas";
+import LocalDiningIcon from "@mui/icons-material/LocalDining";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import CakeIcon from "@mui/icons-material/Cake";
+import NightlifeIcon from "@mui/icons-material/Nightlife";
 
 const OrderPage = () => {
   const dispatch = useDispatch();
@@ -28,13 +39,18 @@ const OrderPage = () => {
   const handleOpenConfirmationDialog = () => {
     dispatch({ type: SET_DIALOG_OPEN, payload: true });
   };
+  const handleSearch = (searchTerm) => {
+    // Implement your search logic here
+    console.log("Searching for:", searchTerm);
+  };
 
-  const StyledPaper = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper,
-    ...theme.typography.h2,
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body1,
     padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+    // textAlign: "center",
+    width: "180px",
+    color: "black",
   }));
 
   return (
@@ -45,25 +61,101 @@ const OrderPage = () => {
         onClose={() => dispatch({ type: SET_DIALOG_OPEN, payload: false })}
       />
       <Typography variant="h5">Order {order.id}</Typography>
-      <Box sx={{ width: "100%" }}>
+      <Box style={{ width: "100%" }}>
         <Stack
           direction="row"
           divider={<Divider orientation="vertical" flexItem />}
           spacing={2}
         >
-          <StyledPaper>Supplier: {order.supplier}</StyledPaper>
-          <StyledPaper>Shipping date: {order.shippingDate}</StyledPaper>
-          <StyledPaper>Total: {order.total}</StyledPaper>
+          <Item>
+            <Typography variant="body1" color="gray">
+              Supplier:
+            </Typography>{" "}
+            <Typography variant="h6">
+              <b>{order.supplier}</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography variant="body1" color="gray">
+              Shipping date:{" "}
+            </Typography>
+            <Typography variant="h6">
+              <b>{order.shippingDate}</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography variant="body1" color="gray">
+              Total:
+            </Typography>
+            <Typography variant="h6">
+              <b>{order.total}</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography variant="body1">Categories</Typography>
+            <Typography
+              variant="h5"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: (theme) => theme.spacing(1),
+              }}
+            >
+              <AcUnitIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <FastfoodIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <IcecreamIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <LunchDiningIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <LocalDiningIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <MenuBookIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <CakeIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              <NightlifeIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+            </Typography>
+          </Item>
+          <Item>
+            <Typography variant="body1" color="gray">
+              Department
+            </Typography>
+            <Typography variant="h6">
+              <b>300-444-678</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography variant="body1" color="gray">
+              Status
+            </Typography>
+            <Typography variant="h6">
+              <b>Awaiting your approval</b>
+            </Typography>
+          </Item>
         </Stack>
       </Box>
-
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleOpenConfirmationDialog}
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "20px",
+          marginTop: "20px",
+          gap: "20px",
+        }}
       >
-        <AddIcon /> Add item
-      </Button>
+        <TextField
+          style={{ borderRadius: 20, width: "50%" }}
+          label="Search"
+          variant="outlined"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOpenConfirmationDialog}
+        >
+          <AddIcon /> Add item
+        </Button>
+      </Box>
+
       <OrderList />
       <Button variant="contained" color="primary" onClick={handleApproveOrder}>
         Approve Order
