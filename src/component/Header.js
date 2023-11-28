370026247d7e20e949177645773cb5abad3b1784
vishal kr. singh <82654082+glitchvishal@users.noreply.github.com>
2023-11-28 07:14:10 +0530

    navbar fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,21 +6,24 @@ import {
   IconButton,
   Typography,
   Button,
+  Container,
 } from "@material-ui/core";
 import { Menu as MenuIcon } from "@material-ui/icons";
 
 const Header = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          RecoCo
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
+      <Container>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            RecoCo
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
--- src/component/OrderPage.js
+++ src/component/OrderPage.js
@@ -27,6 +27,24 @@ import LocalDiningIcon from "@mui/icons-material/LocalDining";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import CakeIcon from "@mui/icons-material/Cake";
 import NightlifeIcon from "@mui/icons-material/Nightlife";
+const BreadcrumbsItem = styled(Typography)(({ theme }) => ({
+  cursor: "pointer",
+  "&:hover": {
+    textDecoration: "underline",
+  },
+}));
+const Breadcrumbs = ({ crumbs }) => {
+  return (
+    <Stack direction="row" spacing={1}>
+      {crumbs.map((crumb, index) => (
+        <React.Fragment key={index}>
+          <BreadcrumbsItem>{crumb}</BreadcrumbsItem>
+          {index < crumbs.length - 1 && <Typography>/</Typography>}
+        </React.Fragment>
+      ))}
+    </Stack>
+  );
+};
 
 const OrderPage = () => {
   const dispatch = useDispatch();
@@ -54,113 +72,150 @@ const OrderPage = () => {
   }));
 
   return (
-    <Container>
+    <>
       <Header />
       <ConfirmationDialog
         open={dialogOpen}
         onClose={() => dispatch({ type: SET_DIALOG_OPEN, payload: false })}
       />
-      <Typography variant="h5">Order {order.id}</Typography>
-      <Box style={{ width: "100%" }}>
-        <Stack
-          direction="row"
-          divider={<Divider orientation="vertical" flexItem />}
-          spacing={2}
-        >
-          <Item>
-            <Typography variant="body1" color="gray">
-              Supplier:
-            </Typography>{" "}
-            <Typography variant="h6">
-              <b>{order.supplier}</b>
-            </Typography>
-          </Item>
-          <Item>
-            <Typography variant="body1" color="gray">
-              Shipping date:{" "}
-            </Typography>
-            <Typography variant="h6">
-              <b>{order.shippingDate}</b>
-            </Typography>
-          </Item>
-          <Item>
-            <Typography variant="body1" color="gray">
-              Total:
-            </Typography>
-            <Typography variant="h6">
-              <b>{order.total}</b>
-            </Typography>
-          </Item>
-          <Item>
-            <Typography variant="body1">Categories</Typography>
-            <Typography
-              variant="h5"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: (theme) => theme.spacing(1),
-              }}
-            >
-              <AcUnitIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <FastfoodIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <IcecreamIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <LunchDiningIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <LocalDiningIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <MenuBookIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <CakeIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-              <NightlifeIcon sx={{ padding: (theme) => theme.spacing(1) }} />
-            </Typography>
-          </Item>
-          <Item>
-            <Typography variant="body1" color="gray">
-              Department
-            </Typography>
-            <Typography variant="h6">
-              <b>300-444-678</b>
-            </Typography>
-          </Item>
-          <Item>
-            <Typography variant="body1" color="gray">
-              Status
-            </Typography>
-            <Typography variant="h6">
-              <b>Awaiting your approval</b>
-            </Typography>
-          </Item>
-        </Stack>
-      </Box>
       <Box
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "20px",
           marginTop: "20px",
-          gap: "20px",
+          marginBottom: "20px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          padding: "20px",
+          borderRadius: "8px",
         }}
       >
-        <TextField
-          style={{ borderRadius: 20, width: "50%" }}
-          label="Search"
-          variant="outlined"
-          onChange={(e) => handleSearch(e.target.value)}
-        />
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleOpenConfirmationDialog}
-        >
-          <AddIcon /> Add item
-        </Button>
+        <Container>
+          <Breadcrumbs crumbs={["Orders", `Order ${order.id}`]} />
+          <Box
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: "20px",
+            }}
+          >
+            <Typography variant="h5">Order {order.id}</Typography>
+            <Stack spacing={2} direction="row">
+              <Button variant="outlined" color="primary">
+                Back
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleApproveOrder}
+              >
+                Approve Order
+              </Button>
+            </Stack>
+          </Box>
+        </Container>
       </Box>
+      <Container>
+        <Box style={{ width: "100%" }}>
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={2}
+          >
+            <Item>
+              <Typography variant="body1" color="gray">
+                Supplier:
+              </Typography>{" "}
+              <Typography variant="h6">
+                <b>{order.supplier}</b>
+              </Typography>
+            </Item>
+            <Item>
+              <Typography variant="body1" color="gray">
+                Shipping date:{" "}
+              </Typography>
+              <Typography variant="h6">
+                <b>{order.shippingDate}</b>
+              </Typography>
+            </Item>
+            <Item>
+              <Typography variant="body1" color="gray">
+                Total:
+              </Typography>
+              <Typography variant="h6">
+                <b>{order.total}</b>
+              </Typography>
+            </Item>
+            <Item>
+              <Typography variant="body1">Categories</Typography>
+              <Typography
+                variant="h5"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: (theme) => theme.spacing(1),
+                }}
+              >
+                <AcUnitIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+                <FastfoodIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+                <IcecreamIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+                <LunchDiningIcon
+                  sx={{ padding: (theme) => theme.spacing(1) }}
+                />
+                <LocalDiningIcon
+                  sx={{ padding: (theme) => theme.spacing(1) }}
+                />
+                <MenuBookIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+                <CakeIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+                <NightlifeIcon sx={{ padding: (theme) => theme.spacing(1) }} />
+              </Typography>
+            </Item>
+            <Item>
+              <Typography variant="body1" color="gray">
+                Department
+              </Typography>
+              <Typography variant="h6">
+                <b>300-444-678</b>
+              </Typography>
+            </Item>
+            <Item>
+              <Typography variant="body1" color="gray">
+                Status
+              </Typography>
+              <Typography variant="h6">
+                <b>Awaiting your approval</b>
+              </Typography>
+            </Item>
+          </Stack>
+        </Box>
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "20px",
+            marginTop: "20px",
+            gap: "20px",
+          }}
+        >
+          <TextField
+            style={{ borderRadius: "50px", width: "50%" }}
+            label="Search"
+            variant="outlined"
+            onChange={(e) => handleSearch(e.target.value)}
+          />
 
-      <OrderList />
-      <Button variant="contained" color="primary" onClick={handleApproveOrder}>
-        Approve Order
-      </Button>
-    </Container>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpenConfirmationDialog}
+          >
+            <AddIcon /> Add item
+          </Button>
+        </Box>
+
+        <OrderList />
+      </Container>
+    </>
   );
 };
 
